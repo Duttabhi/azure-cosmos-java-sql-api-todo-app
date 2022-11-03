f4ff1dcba9074673fb8ae4ec8d222f111805c6e6
Abhishek Dutta <35317861+Duttabhi@users.noreply.github.com>
2022-11-04 02:03:50 +0530

    updating credentials

--- src/com/microsoft/azure/cosmos/sample/dao/CosmosClientFactory.java
+++ src/com/microsoft/azure/cosmos/sample/dao/CosmosClientFactory.java
@@ -5,8 +5,8 @@ import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosClientBuilder;
 
 public class CosmosClientFactory {
-    private static final String HOST = "[ACCOUNT HOST NAME]";
-    private static final String MASTER_KEY = "[ACCOUNT KEY]";
+    private static final String HOST = "https://myprojectcosmosdb.documents.azure.com:443/";
+    private static final String MASTER_KEY = "FhBqQBPYXWhzaIyDtDPx39ydulDsR4Hcpw5HgnaBHPGYJaiOUwLXzFztVgQZHbUM35HsRvXayJmYACDbq881Vg==";
 
     private static CosmosClient cosmosClient = new CosmosClientBuilder()
             .endpoint(HOST)
